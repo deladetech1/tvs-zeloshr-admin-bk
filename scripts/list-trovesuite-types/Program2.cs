97fed018dd712b633fa77a10b348efcd00ad7041
isaackumi-deladetech <isaac.kumi@deladetech.com>
2026-05-25 20:51:36 +0000

    chore: remove accidental debug script from repo

--- scripts/list-trovesuite-types/Program2.cs
+++
@@ -1,5 +0,0 @@
-using System.Reflection;
-var asm = Assembly.LoadFrom("/root/.nuget/packages/trovesuite.package/1.0.0/lib/net10.0/Trovesuite.Package.dll");
-var auth = asm.GetType("Trovesuite.Package.Auth.IAuthService")!;
-foreach (var m in auth.GetMethods())
-  Console.WriteLine(m.ToString());
